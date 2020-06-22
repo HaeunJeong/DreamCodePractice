f2ed75c76427cbdd8394e6a0462f8bb2ee4a0d95
HaeunJeong <nicehe74@gmail.com>
2020-06-23 00:56:22 +0900

    second

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-function filterByColor(color){
-    switch(color){
-        case Blue: 
+let colors = ['blue', 'pink', 'yellow'];
+let kinds = ['p','s','t'];
+let size = ['large', 'small'];
+let sex = ['male', 'female'];
+
+class Clothes {
+    constructor(colors, kinds, size, sex){
+        this.colors = colors;
+        this.kinds = kinds;
+        this.size = size;
+        this.sex = sex;
     }
+}
+const piece = new Clothes('')
+const clothes = new Array(Clothes);
+
+
+let filteredClothes = clothes.filter(filterByColor);
+
+function filterByColor(value, index, array){
+    return 
 
 }
